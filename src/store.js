fc859b42569b46bc5ef23c47e545028307fa6181
yubathom <yubathom@gmail.com>
2020-06-22 21:57:16 -0300

    style: lint store

--- src/store.js
+++ src/store.js
@@ -12,7 +12,7 @@ export default new Vuex.Store({
     rowgap: 0,
     colArr: [],
     rowArr: [],
-    childarea: []
+    childarea: [],
   },
   getters: {
     colTemplate(state) {
@@ -29,23 +29,22 @@ export default new Vuex.Store({
   },
   mutations: {
     initialArrIndex(state, payload) {
-      if(payload !== '') {
-        const queryParams = new URLSearchParams(payload)
+      if (payload !== "") {
+        const queryParams = new URLSearchParams(payload);
 
         for (const stateKey in state) {
-          const paramIsValid = queryParams.has(stateKey)
-          const paramType = typeof(state[stateKey])
+          const paramIsValid = queryParams.has(stateKey);
+          const paramType = typeof state[stateKey];
 
-          if(paramIsValid && paramType === 'number') {
+          if (paramIsValid && paramType === "number") {
             state[stateKey] = queryParams.get(stateKey);
-          }
-          else if (paramIsValid && paramType === 'object') {
-            state[stateKey] = JSON.parse(queryParams.get(stateKey))
+          } else if (paramIsValid && paramType === "object") {
+            state[stateKey] = JSON.parse(queryParams.get(stateKey));
           }
         }
       } else {
-          createArr(state.columns, state.colArr);
-          createArr(state.rows, state.rowArr);
+        createArr(state.columns, state.colArr);
+        createArr(state.rows, state.rowArr);
       }
     },
     adjustArr(state, payload) {
@@ -85,8 +84,8 @@ export default new Vuex.Store({
     },
     resetGrid(state, payload) {
       state.childarea = [];
-    }
-  }
+    },
+  },
 });
 
 //we start off with just a few rows and columns filled with 1fr units
